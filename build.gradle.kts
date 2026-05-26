eb528e943dc2774ba5358728cea440ed2d4b063b
Muye Chen <muyechen2026@u.northwestern.edu>
2026-05-26 14:08:05 -0500

    pitest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
+import com.github.spotbugs.snom.Confidence
+import com.github.spotbugs.snom.Effort
+
 plugins {
     id("java")
+    jacoco
+    application
+    checkstyle
+    id("com.github.spotbugs") version "6.0.25"
+//    id("java") //or 'java-library' - depending on your needs
+    id("info.solidsoft.pitest") version "1.19.0"
 }
 
 group = "nu.csse.sqe"
@@ -7,6 +16,14 @@ version = "1.0"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass = "domain"
+}
+
+tasks.build {
+    dependsOn("pitest")
 }
 
 dependencies {
@@ -23,7 +40,83 @@ java {
 tasks.compileJava {
     options.release = 11
 }
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required = false
+        html.required = true
+        html.stylesheet = resources.text.fromFile("config/xsl/checkstyle-noframes-severity-sorted.xsl")
+    }
+}
+checkstyle{
+    toolVersion = "10.12.5"
+    isIgnoreFailures = false
+    configFile = file("config/checkstyle/checkstyle.xml")
+}
 
 tasks.test {
     useJUnitPlatform()
+}
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)// report is always generated after tests run
+    finalizedBy(tasks.pitest)
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+jacoco {
+    toolVersion = "0.8.14"
+    reportsDirectory = layout.buildDirectory.dir("customJacocoReportDir")
+}
+spotbugs {
+    ignoreFailures = false
+    showStackTraces = true
+    showProgress = true
+    effort = Effort.DEFAULT
+    reportLevel = Confidence.DEFAULT
+    //omitVisitors = listOf("FindNonShortCircuit")
+    reportsDir = file("spotbugs")
+    //onlyAnalyze = listOf("com.foobar.MyClass", "com.foobar.mypkg.*")
+    maxHeapSize = "1g"
+    extraArgs = listOf("-nested:false")
+    //jvmArgs = listOf("-Duser.language=ja") // set user language to japanese
+}
+
+tasks.spotbugsMain {
+    reports.create("html") {
+        required = true
+        outputLocation = layout.buildDirectory.file("reports/spotbugs/spotbugs.html")
+        setStylesheet("fancy-hist.xsl")
+    }
+}
+tasks.jacocoTestReport {
+    reports {
+        xml.required = false
+        csv.required = false
+        html.outputLocation = layout.buildDirectory.dir("jacocoHtml")
+    }
+}
+jacoco {
+    applyTo(tasks.run.get())
+}
+
+tasks.register<JacocoReport>("applicationCodeCoverageReport") {
+    executionData(tasks.run.get())
+    sourceSets(sourceSets.main.get())
+}
+
+pitest {
+    targetClasses = setOf("domain.*") //by default "${project.group}.*"
+    targetTests = setOf("domain.*")
+    junit5PluginVersion = "1.2.1"
+    pitestVersion = "1.19.0" //not needed when a default PIT version should be used
+
+    threads = 4
+    outputFormats = setOf("HTML")
+    timestampedReports = false
+    testSourceSets.set(listOf(sourceSets.test.get()))
+    mainSourceSets.set(listOf(sourceSets.main.get()))
+    jvmArgs.set(listOf("-Xmx1024m"))
+    useClasspathFile.set(true) //useful with bigger projects on Windows
+    fileExtensionsToFilter.addAll("xml")
+    exportLineCoverage = true
 }
