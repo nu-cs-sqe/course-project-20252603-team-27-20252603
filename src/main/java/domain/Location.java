c1d3a3adb806ac9df54fe221bc8f6c7d3e19598a
Bizimana61 <jeromebizimana2027@u.northwestern.edu>
2026-05-29 20:36:19 -0500

    Implemented Location.equals(object) and hashCode

--- src/main/java/domain/Location.java
+++ src/main/java/domain/Location.java
@@ -78,4 +78,24 @@ public final class Location {
 	public int getCol() {
 		return col;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Location)) {
+			return false;
+		}
+		Location other = (Location) obj;
+		return this.row == other.row && this.col == other.col;
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 * row + col;
+	}
 }
